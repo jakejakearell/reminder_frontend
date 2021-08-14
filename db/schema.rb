f896e3ec8ee7220946651d37ad1efc658b41831b
Jacob Arellano <jmm.arellano@gmail.com>
2021-08-14 14:17:03 -0600

    Add timestamps, emails to user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_193958) do
+ActiveRecord::Schema.define(version: 2021_08_14_201554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2021_08_14_193958) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
