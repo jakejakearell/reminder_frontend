8576d294a5c49b74daafae97b06f39f5cccd8119
Jacob Arellano <jmm.arellano@gmail.com>
2021-08-14 14:19:05 -0600

    Add validations for user emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates_presence_of :password, require: true
 
   has_secure_password
