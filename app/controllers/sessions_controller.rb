ba619fd771f36e670982993984f3c94170c987fc
Jacob Arellano <jmm.arellano@gmail.com>
2021-08-17 12:39:29 -0600

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email].downcase)
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_index_path
+    else
+      flash[:message] = 'The email or password you entered is incorrect'
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
