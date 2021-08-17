1965b6688b46df1fb117fc77e24acd014994ccee
Jacob Arellano <jmm.arellano@gmail.com>
2021-08-17 10:08:13 -0600

    Add create action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    new_user = User.new(user_params)
+    if new_user.save
+      flash[:message] = "Welcome to Viewing Party, #{new_user.email}"
+      session[:user_id] = new_user.id
+      redirect_to dashboard_index_path
+    else
+      flash[:error] = new_user.errors.full_messages.to_sentence
+      @user = User.new
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params[:user][:email] = params[:user][:email].downcase
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
