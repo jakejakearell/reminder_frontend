3bd8b79f9c1485afd017b3c05967c1f4a718ff31
Jacob Arellano <jmm.arellano@gmail.com>
2021-09-07 15:48:54 -0600

    Add param checks

--- app/facades/reminder_facade.rb
+++ app/facades/reminder_facade.rb
@@ -1,27 +1,33 @@
 class ReminderFacade
+  attr_reader :errors
+
   def initialize(params, user)
     @params = params
     @user = user
-  end
-
-  def save
+    @errors = ""
   end
 
   def valid?
-    if valid_date? && valid_time? && valid_message?
+    if check_all_valid
       true
     else
       false
     end
   end
 
+  def check_all_valid
+    checks = [valid_date?, valid_time?, valid_message?]
+    checks.all? { |check| check == true}
+  end
+
   def valid_date?
     start_date = @params[:start_date]
     end_date = @params[:end_date]
-
     if start_date.empty? || end_date.empty?
+      @errors += "Date Can't be Blank, "
       false
     elsif Date.parse(start_date) >= Date.parse(end_date)
+      @errors += "Start Date Can't be Greater than end date, "
       false
     else
       true
@@ -29,10 +35,20 @@ class ReminderFacade
   end
 
   def valid_time?
-    !@params[:time].empty? ? true : false
+    if @params[:time].empty?
+      @errors += "Must select a time, "
+      false
+    else
+      true
+    end
   end
 
   def valid_message?
-    !@params[:message].empty? ? true : false
+    if @params[:message].empty?
+      @errors += "Message Can't be empty, "
+      false
+    else
+      true
+    end
   end
 end
