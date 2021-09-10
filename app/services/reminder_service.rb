207bf4347fb68c2514a9d5669189fa141ad6d2e4
Jacob Arellano <jmm.arellano@gmail.com>
2021-09-10 09:45:46 -0600

    Add a method to post reminders

--- app/services/reminder_service.rb
+++ app/services/reminder_service.rb
@@ -1,6 +1,14 @@
-class ReminderService 
+class ReminderService
+  def self.post_reminder(reminder)
+    response = conn.post('/api/v1/reminder') do |request|
+      request.headers['Content-Type'] = 'application/json'
+      request.body = reminder.to_json
+    end
+  end
+
   private
-  def conn
-    Faraday.new(url: 'localhost:3000')
+
+  def self.conn
+    Faraday.new(url: 'http://localhost:3000')
   end
 end
